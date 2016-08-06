e60458cb385ac7114bd7191facbc3a0ad1d5858c
teszta <tsz@pofa.be>
2016-08-06 23:26:56 +0200

    1.0
    TClean first release
    -Analyze option to search for torrent files in a specicif folder
    -Clean for delete torrent files
    -Error log if path not found
    -List  of actual torrent files

---
+++ TClean/TClean.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace TClean
+{
+    partial class TClean
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TClean));
+            this.Lista = new System.Windows.Forms.ListBox();
+            this.Analyze = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Clean = new System.Windows.Forms.Button();
+            this.TargetBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Lista
+            // 
+            this.Lista.FormattingEnabled = true;
+            this.Lista.Items.AddRange(new object[] {
+            "Ready"});
+            this.Lista.Location = new System.Drawing.Point(12, 108);
+            this.Lista.Name = "Lista";
+            this.Lista.Size = new System.Drawing.Size(593, 134);
+            this.Lista.TabIndex = 0;
+            // 
+            // Analyze
+            // 
+            this.Analyze.Location = new System.Drawing.Point(515, 16);
+            this.Analyze.Name = "Analyze";
+            this.Analyze.Size = new System.Drawing.Size(90, 35);
+            this.Analyze.TabIndex = 1;
+            this.Analyze.Text = "Analyze";
+            this.Analyze.UseVisualStyleBackColor = true;
+            this.Analyze.Click += new System.EventHandler(this.Analyze_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Target:";
+            // 
+            // Clean
+            // 
+            this.Clean.Location = new System.Drawing.Point(515, 57);
+            this.Clean.Name = "Clean";
+            this.Clean.Size = new System.Drawing.Size(90, 38);
+            this.Clean.TabIndex = 3;
+            this.Clean.Text = "Clean";
+            this.Clean.UseVisualStyleBackColor = true;
+            this.Clean.Click += new System.EventHandler(this.Clean_Click);
+            // 
+            // TargetBox
+            // 
+            this.TargetBox.Location = new System.Drawing.Point(56, 31);
+            this.TargetBox.Name = "TargetBox";
+            this.TargetBox.Size = new System.Drawing.Size(369, 20);
+            this.TargetBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Status:";
+            // 
+            // TClean
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 273);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TargetBox);
+            this.Controls.Add(this.Clean);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Analyze);
+            this.Controls.Add(this.Lista);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "TClean";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TClean";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox Lista;
+        private System.Windows.Forms.Button Analyze;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Clean;
+        private System.Windows.Forms.TextBox TargetBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TClean/TClean.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TClean
+{
+    public partial class TClean : Form
+    {
+        public TClean()
+        {
+            InitializeComponent();
+            string targetdef = "C:\\Users\\" + Environment.UserName + "\\Downloads";
+            TargetBox.Text = targetdef;
+        }
+
+        private void Analyze_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Directory.GetFiles(TargetBox.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Target must be a valid path!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                StreamWriter sw = new StreamWriter("error.log");
+                sw.Write(ex + "\n\n");
+                sw.Close();
+                TargetBox.Focus();
+                return;
+            }
+            Lista.Items.Clear();
+            object[] fajlok = Directory.GetFiles(TargetBox.Text, "*.torrent");
+            int count = 0;
+            if (fajlok.Count() == 0)
+            {
+                Lista.Items.Add("There are no torrent files!");
+            }
+            else
+            {
+                for (int i = 0; i < fajlok.Length; i++)
+                {
+                    string s = (string)fajlok[i];
+                    string szoveg = s.Substring(s.LastIndexOf('\\') + 1);
+                    Lista.Items.Add((szoveg));
+                    count++;
+                }
+                Lista.Items.Add("..");
+                Lista.Items.Add("Analyze completed." + count + " items were ready to clean.");
+            }
+        }
+
+        private void Clean_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Directory.GetFiles(TargetBox.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Target must be a valid path!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                StreamWriter sw = new StreamWriter("error.log");
+                sw.Write(ex+"\n\n");
+                sw.Close();
+                TargetBox.Focus();
+                return;
+            }
+            Lista.Items.Clear();
+            object[] fajlok = Directory.GetFiles(TargetBox.Text, "*.torrent");
+            if (fajlok.Count() == 0)
+            {
+                Lista.Items.Add("There are no torrent files!");
+            }
+            else
+            {
+                DialogResult result = MessageBox.Show("Are you sure?\nIt will remove " + fajlok.Count() + " files.", "Clean", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2);
+                if (result == DialogResult.No || result == DialogResult.Cancel)
+                {
+                    Lista.Items.Add("Aborted..");
+                }
+                else
+                {
+                    Lista.Items.Add("Deleting");
+                    Lista.Items.Add("..");
+                    int count = 0;
+                    for (int i = 0; i < fajlok.Length; i++)
+                    {
+                        string s = (string)fajlok[i];
+                        string szoveg = s.Substring(s.LastIndexOf('\\') + 1);
+                        Lista.Items.Add((szoveg));
+                        File.Delete(s);
+                        count++;
+                    }
+                    Lista.Items.Add("..");
+                    Lista.Items.Add("Cleaning completed." + count + " items were removed.");
+                }
+            }
+        }
+    }
+}
